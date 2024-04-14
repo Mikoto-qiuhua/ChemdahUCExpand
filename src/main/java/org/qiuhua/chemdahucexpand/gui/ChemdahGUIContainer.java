b3fe9906851b8fb59e31126f6f33ea613dbebb0c
Mikoto_qiuhua <215745352@outlook.com>
2024-04-14 12:34:56 +0800

    修正打字机动画效果

--- src/main/java/org/qiuhua/chemdahucexpand/gui/ChemdahGUIContainer.java
+++ src/main/java/org/qiuhua/chemdahucexpand/gui/ChemdahGUIContainer.java
@@ -12,6 +12,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitScheduler;
 import org.bukkit.scheduler.BukkitTask;
 import org.qiuhua.chemdahucexpand.Main;
 import org.qiuhua.chemdahucexpand.config.Config;
@@ -94,87 +95,139 @@ public class ChemdahGUIContainer extends UnrealCoreGUI {
                 if (mouseButtonType == MouseButtonType.Left && mouseActionType == MouseActionType.Off) {
                     player.playSound(player.getLocation(),sound,volume,pitch);
                     player.chat("/session reply " + reply.getRid());
+                    boolean isTalk = false;
+                    boolean isClose = false;
+                    for(String action : reply.getAction()){
+                        if(action.contains("talk")){
+                            isTalk = true;
+                        }
+                        if(action.contains("close")){
+                            isClose = true;
+                        }
+                    }
+                    //如果有close没有talk并且没有回复选项 那就立即关闭界面
+                    if(isClose && !isTalk){
+                        this.close();
+                    }
                 }
             });
             this.addModule(buttonModule);
+            this.upDate();
         }
     }
 
 
-//    //设置文本组件
-//    public void setMessage(){
-//        //获取消息组件并且设置文本
-//        TextModule messageModule = (TextModule)this.getModule(new String[]{"Message"});
-//        if(messageModule != null) {
-//            messageModule.setText(message);
-//        }
-//    }
 
-    public void setMessage(List<String> message){
+    public void setMessage(List<String> display){
         //获取消息组件并且设置文本
         TextModule messageModule = (TextModule)this.getModule(new String[]{"Message"});
         if(messageModule != null) {
-            messageModule.setText(message);
+            messageModule.setText(display);
         }
     }
+
+
+    //当前文本播放的索引
+    private Integer megIndex = 0;
+    //动态消息列表
+    private final List<String> newMeg = new ArrayList<>();
+    //消息播放速度
+    private final Integer messagePlaySpeed = Config.getInt("Option.messagePlaySpeed");
 
 
     //界面打开时自动触发
     public void opening() {
-        typeWriter(0,1000L, (unused) -> {
-
-        });
-        //如果文本对话完成就设置回复选项
-        this.setPlayerReplyList();
-
-        //如果是最后一句回复 那就延迟关闭界面
-        if(!canReply){
-            Bukkit.getScheduler().runTaskLaterAsynchronously(Main.getMainPlugin(), () -> {
-                Bukkit.getScheduler().runTask(Main.getMainPlugin(), this::close);
-            }, talkCloseDelay);
+        List<List<String>> a = new ArrayList<>();
+        for (String s : this.message) {
+            a.add(splitText(s));
         }
+        typeWriter(a, (unused) -> {
+            //如果玩家没有的回复 那就延迟关闭界面
+            if(!canReply){
+                Bukkit.getScheduler().runTaskLaterAsynchronously(Main.getMainPlugin(), () -> {
+                    Bukkit.getScheduler().runTask(Main.getMainPlugin(), this::close);
+                }, talkCloseDelay);
+                return;
+            }
+            //如果文本对话完成就设置回复选项
+            this.setPlayerReplyList();
+        });
+
+
+
 
     }
+
+    private BukkitTask task;
 
     /**
-     * 打字效果
-     * @param startDelay 延迟多久后开始
-     * @param period 显示间隔
-     * @param end 显示完毕后干的事
+     *
+     * @param a 传递的递归数组
+     * @param end 结束时执行的代码
      */
-    public void typeWriter (long startDelay, long period, Consumer<Void> end)
-    {
-        List<String> display = new ArrayList<>();
-        message.forEach((oo) -> display.add(""));
-        for (int i = 0; i < message.size(); i++)
-        {
-            char[] words = message.get(i).toCharArray();
-            final int[] j = {0};
-            int finalI = i;
-            Bukkit.getScheduler().runTaskTimerAsynchronously(Main.getMainPlugin(), (task) -> {
-
-                if (j[0] < words.length) {
-
-                    if (j[0] == 0)
-                        display.set(finalI, words[j[0]] + "_");
-                    else {
-                        display.set(finalI, display.get(finalI).replace("_", words[j[0]] + "_"));
-                    }
-
-                    j[0]++;
-                    setMessage(display);
-                    upDate();
-                }else
+    public void typeWriter (List<List<String>> a, Consumer<Void> end) {
+        Bukkit.getScheduler().runTaskTimerAsynchronously(Main.getMainPlugin(), (task) -> {
+            if(a.isEmpty()){
+                task.cancel();
+                end.accept(null);
+                return;
+            }
+            this.task = task;
+            //每次都取出递归数组中的第一个元素进行索引  也就是第一行文本
+            List<String> splitText = a.get(0);
+            //如果这个元素是空的那代表里面的文本已经播放完毕 把他给删除 并且进行换行操作
+            if(splitText.isEmpty()){
+                //移除这个元素 避免下次重复拿取
+                a.remove(0);
+                //换行操作
+                ++ megIndex;
+                if(a.isEmpty()){
                     task.cancel();
-            }, startDelay, period);
+                    end.accept(null);
+                    return;
+                }
+                //这里要重新拿取新的
+                splitText = a.get(0);
+            }
+            //拿到这行文本的第一个字符 如果这是最后一个字符就不加下划线
+            String meg = splitText.get(0);
+            if(splitText.size() != 1){
+                meg = meg + "_";
+            }
+            //先看newMeg内有没有这个行数
+            //如果他的大小是当前输出的消息行数大小 则代表没有该元素
+            if(newMeg.size() == megIndex){
+                newMeg.add(meg);
+            }else{
+                //拼接字符串前先删掉多余的下划线
+                String meg0 = newMeg.get(megIndex).replace("_", "");
+                //否则就拼接字符串
+                newMeg.set(megIndex, meg0 + meg);
+            }
+            splitText.remove(0);
+            setMessage(newMeg);
 
-        }
-
-        end.accept(null);
-
+            //更新界面
+            this.upDate();
+        }, 0, messagePlaySpeed);
     }
 
+    private static List<String> splitText(String text) {
+        List<String> outputList = new ArrayList<>();
 
+        if (text.length() == 1) {
+            outputList.add(text);
+        } else {
+            String firstChar = text.substring(0, 1);
+            outputList.add(firstChar);
+
+            String remainingChars = text.substring(1);
+            List<String> recursiveList = splitText(remainingChars);
+            outputList.addAll(recursiveList);
+        }
+
+        return outputList;
+    }
 
 
 
@@ -183,6 +236,9 @@ public class ChemdahGUIContainer extends UnrealCoreGUI {
     public void close() {
         //这里是断开本次会话连接
         this.session.close(true);
+        if(task != null){
+            this.task.cancel();
+        }
         super.close();
     }
 
